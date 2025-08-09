cf59f8fe7ec86f890b9b72201114c38a85151afd
hyeongbeom-kim <leopard2@wizvil.com>
2025-08-09 17:59:46 +0900

    1st commit

---
+++ src/main/java/panzer/leopard2/openai2/Openai2Application.java
@@ -0,0 +1,13 @@
+package panzer.leopard2.openai2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openai2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Openai2Application.class, args);
+    }
+
+}
---
+++ src/test/java/panzer/leopard2/openai2/Openai2ApplicationTests.java
@@ -0,0 +1,13 @@
+package panzer.leopard2.openai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Openai2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
